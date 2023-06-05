7b388e46f00d9c54f64f66d96ff95b818fc54a3f
Emancepado <100377421+Emancepado@users.noreply.github.com>
2023-06-05 13:03:29 -0500

    Segunda Pre Entrega ManceboFranco

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Segunda Entrega",
+    version="1.1",
+    description="Registro de usuarios con listado de productos, ergo, carrito",
+    author="Franco Mancebo",
+    author_email="francoqui2212@gmail.com",
+    packages=["Python"]
+)
